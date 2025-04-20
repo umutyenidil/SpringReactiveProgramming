f3f30f7ae609028ec1663c1a2a47e4f9151f3e51
Umut Yenidil <umut.yenidil9@gmail.com>
2025-04-20 22:42:31 +0300

    proje olusturuldu

---
+++ src/main/java/com/umutyenidil/springreactiveprogramming/SpringReactiveProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.umutyenidil.springreactiveprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveProgrammingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/umutyenidil/springreactiveprogramming/SpringReactiveProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umutyenidil.springreactiveprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveProgrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
